9d0e5286c3c01e31c089fe65c4f399bcd5442840
Ilya Lukashevich <lukashevich.ia@phystech.edu>
2020-11-26 20:25:38 +0300

    changed notebook and utils files

--- Classification/utils.py
+++ Classification/utils.py
@@ -11,43 +11,17 @@ from torch.utils.data.sampler import SubsetRandomSampler
 PLOT_STEP = 0
 
 
-def get_train_valid_loader(data_dir,
-                           batch_size,
-                           valid_size=0.2,
-                           num_workers=4,
-                           random_seed=42,
-                           augment=False,
-                           shuffle=True,
-                           pin_memory=True):
-    """
-    Utility function for loading and returning train and valid
-    multi-process iterators over the CIFAR-10 dataset. A sample
-    9x9 grid of the images can be optionally displayed.
-
-    If using CUDA, num_workers should be set to 1 and pin_memory to True.
-
-    Params
-    ------
-    - data_dir: path directory to the dataset.
-    - batch_size: how many samples per batch to load.
-    - augment: whether to apply the data augmentation scheme
-      mentioned in the paper. Only applied on the train split.
-    - random_seed: fix seed for reproducibility.
-    - valid_size: percentage split of the training set used for
-      the validation set. Should be a float in the range [0, 1].
-    - shuffle: whether to shuffle the train/validation indices.
-    - show_sample: plot 9x9 sample grid of the dataset.
-    - num_workers: number of subprocesses to use when loading the dataset.
-    - pin_memory: whether to copy tensors into CUDA pinned memory. Set it to
-      True if using GPU.
-
-    Returns
-    -------
-    - train_loader: training set iterator.
-    - valid_loader: validation set iterator.
-    """
+def get_train_val_loader(data_dir,
+                         batch_size,
+                         train_transform=None,
+                         val_transform=None,
+                         val_size=0.2,
+                         num_workers=4,
+                         random_seed=42,
+                         shuffle=True,
+                         pin_memory=True):
     error_msg = "[!] valid_size should be in the range [0, 1]."
-    assert ((valid_size >= 0) and (valid_size <= 1)), error_msg
+    assert ((val_size >= 0) and (val_size <= 1)), error_msg
 
     normalize = transforms.Normalize(
         mean=[0.4914, 0.4822, 0.4465],
@@ -55,19 +29,13 @@ def get_train_valid_loader(data_dir,
     )
 
     # define transforms
-    valid_transform = transforms.Compose([
-        transforms.ToTensor(),
-        normalize,
-    ])
-
-    if augment:
-        train_transform = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
+    if val_transform is None:
+        val_transform = transforms.Compose([
             transforms.ToTensor(),
             normalize,
         ])
-    else:
+
+    if train_transform is None:
         train_transform = transforms.Compose([
             transforms.ToTensor(),
             normalize,
@@ -79,74 +47,55 @@ def get_train_valid_loader(data_dir,
         download=True, transform=train_transform,
     )
 
-    valid_dataset = datasets.CIFAR10(
+    val_dataset = datasets.CIFAR10(
         root=data_dir, train=True,
-        download=True, transform=valid_transform,
+        download=True, transform=val_transform,
     )
 
     num_train = len(train_dataset)
     indices = list(range(num_train))
-    split = int(np.floor(valid_size * num_train))
+    split = int(np.floor(val_size * num_train))
 
     if shuffle:
         np.random.seed(random_seed)
         np.random.shuffle(indices)
 
-    train_idx, valid_idx = indices[split:], indices[:split]
+    train_idx, val_idx = indices[split:], indices[:split]
     train_sampler = SubsetRandomSampler(train_idx)
-    valid_sampler = SubsetRandomSampler(valid_idx)
+    val_sampler = SubsetRandomSampler(val_idx)
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=batch_size, sampler=train_sampler,
         num_workers=num_workers, pin_memory=pin_memory,
     )
 
-    valid_loader = torch.utils.data.DataLoader(
-        valid_dataset, batch_size=batch_size, sampler=valid_sampler,
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=batch_size, sampler=val_sampler,
         num_workers=num_workers, pin_memory=pin_memory,
     )
 
-    return (train_loader, valid_loader)
+    return train_loader, val_loader
 
 
 def get_test_loader(data_dir,
                     batch_size,
+                    test_transform=None,
                     num_workers=4,
                     shuffle=True,
                     pin_memory=True):
-    """
-    Utility function for loading and returning a multi-process
-    test iterator over the CIFAR-10 dataset.
-
-    If using CUDA, num_workers should be set to 1 and pin_memory to True.
-
-    Params
-    ------
-    - data_dir: path directory to the dataset.
-    - batch_size: how many samples per batch to load.
-    - shuffle: whether to shuffle the dataset after every epoch.
-    - num_workers: number of subprocesses to use when loading the dataset.
-    - pin_memory: whether to copy tensors into CUDA pinned memory. Set it to
-      True if using GPU.
-
-    Returns
-    -------
-    - data_loader: test set iterator.
-    """
-    normalize = transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225],
-    )
-
     # define transform
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        normalize,
-    ])
+    if test_transform is None:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225],
+            ),
+        ])
 
     dataset = datasets.CIFAR10(
         root=data_dir, train=False,
-        download=True, transform=transform,
+        download=True, transform=test_transform,
     )
 
     data_loader = torch.utils.data.DataLoader(
@@ -157,11 +106,12 @@ def get_test_loader(data_dir,
     return data_loader
 
 
-def train(model, device, iterator, optimizer, criterion, clip, train_history=None,
-          valid_history=None, plot_local=True, writer=None):
+def train(model, device, iterator, optimizer, criterion, clip, train_loss_history=None,
+          valid_loss_history=None, train_acc_history=None, valid_acc_history=None, plot_local=True, writer=None):
     global PLOT_STEP
     model.train()
     epoch_loss = 0
+    train_acc = 0
     history = []
 
     for i, (batch, labels) in enumerate(iterator):
@@ -171,14 +121,23 @@ def train(model, device, iterator, optimizer, criterion, clip, train_history=Non
 
         output = model(batch)
         loss = criterion(output, labels)
+        epoch_loss += loss.item()
         loss.backward()
+
+        # Calculate train accuracy
+        ret, predictions = torch.max(output.data, 1)
+        correct_counts = predictions.eq(labels.data.view_as(predictions))
+
+        # Convert correct_counts to float and then compute the mean
+        acc = torch.mean(correct_counts.type(torch.FloatTensor))
+
+        # Compute total accuracy in the whole batch and add to train_acc
+        train_acc += acc.item()
 
         # Let's clip the gradient
         torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
 
         optimizer.step()
-
-        epoch_loss += loss.item()
 
         history.append(loss.cpu().data.numpy())
         if (i + 1) % 10 == 0:
@@ -187,27 +146,31 @@ def train(model, device, iterator, optimizer, criterion, clip, train_history=Non
                 writer.add_scalar('Train loss', history[-1], PLOT_STEP)
 
             if plot_local:
-                fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 8))
+                fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(18, 8))
 
                 clear_output(True)
                 ax[0].plot(history, label='train loss')
                 ax[0].set_xlabel('Batch')
                 ax[0].set_title('Train loss')
-                if train_history is not None:
-                    ax[1].plot(train_history, label='general train history')
+                if train_loss_history is not None:
+                    ax[1].plot(train_loss_history, label='train loss history')
                     ax[1].set_xlabel('Epoch')
-                if valid_history is not None:
-                    ax[1].plot(valid_history, label='general valid history')
+                    ax[2].plot(train_acc_history, label='train accuracy history')
+                    ax[2].set_xlabel('Epoch')
+                if valid_loss_history is not None:
+                    ax[1].plot(valid_loss_history, label='valid loss history')
+                    ax[2].plot(valid_acc_history, label='valid accuracy history')
                 plt.legend()
 
                 plt.show()
 
-    return epoch_loss / len(iterator)
+    return epoch_loss / len(iterator), train_acc / len(iterator)
 
 
 def evaluate(model, device, iterator, criterion):
     model.eval()
     epoch_loss = 0
+    valid_acc = 0
 
     with torch.no_grad():
         for i, (batch, labels) in enumerate(iterator):
@@ -216,10 +179,19 @@ def evaluate(model, device, iterator, criterion):
             output = model(batch)
 
             loss = criterion(output, labels)
-
             epoch_loss += loss.item()
 
-    return epoch_loss / len(iterator)
+            # Calculate validation accuracy
+            ret, predictions = torch.max(output.data, 1)
+            correct_counts = predictions.eq(labels.data.view_as(predictions))
+
+            # Convert correct_counts to float and then compute the mean
+            acc = torch.mean(correct_counts.type(torch.FloatTensor))
+
+            # Compute total accuracy in the whole batch and add to valid_acc
+            valid_acc += acc.item()
+
+    return epoch_loss / len(iterator), valid_acc / len(iterator)
 
 
 def epoch_time(start_time, end_time):
@@ -231,17 +203,21 @@ def epoch_time(start_time, end_time):
 
 def train_model(model, device, train_iterator, valid_iterator, optimizer, criterion,
                 lr_scheduler, n_epochs, clip, writer=None, model_name="model.pth"):
-    train_history = []
-    valid_history = []
+    train_loss_history = []
+    valid_loss_history = []
+    train_acc_history = []
+    valid_acc_history = []
+
     best_valid_loss = float('inf')
 
     for epoch in range(n_epochs):
 
         start_time = time.time()
 
-        train_loss = train(model, device, train_iterator, optimizer, criterion, clip,
-                           train_history, valid_history, plot_local=True, writer=writer)
-        valid_loss = evaluate(model, device, valid_iterator, criterion)
+        train_loss, train_acc = train(model, device, train_iterator, optimizer, criterion, clip,
+                                      train_loss_history, valid_loss_history, train_acc_history, valid_acc_history,
+                                      plot_local=True, writer=writer)
+        valid_loss, valid_acc = evaluate(model, device, valid_iterator, criterion)
 
         if writer:
             writer.add_scalar('Mean train loss per epoch', train_loss, global_step=epoch)
@@ -258,8 +234,11 @@ def train_model(model, device, train_iterator, valid_iterator, optimizer, criter
             best_valid_loss = valid_loss
             torch.save(model.state_dict(), model_name)
 
-        train_history.append(train_loss)
-        valid_history.append(valid_loss)
+        train_loss_history.append(train_loss)
+        valid_loss_history.append(valid_loss)
+        train_acc_history.append(train_acc)
+        valid_acc_history.append(valid_acc)
+
         print(f'Epoch: {epoch + 1:02} | Time: {epoch_mins}m {epoch_secs}s')
         print(f'\tTrain Loss: {train_loss:.3f}')
         print(f'\t Val. Loss: {valid_loss:.3f}')
